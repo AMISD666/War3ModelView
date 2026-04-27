bf71cb7f0dcb6e4f62734c817d682a275922b7cc
AMISD666 <2530075955@qq.com>
2026-04-28 00:02:26 +0800

    Delete vendor directory

--- vendor/blp-rs/src/error/cause.rs
+++
@@ -1,9 +0,0 @@
-use crate::error::error::BlpError;
-use std::error::Error;
-use std::sync::Arc;
-
-#[derive(Debug, Clone)]
-pub(crate) enum Cause {
-    Blp(BlpError),
-    Std(Arc<dyn Error + Send + Sync>),
-}
--- vendor/blp-rs/src/jpg/mod.rs
+++
@@ -1,4 +0,0 @@
-mod jpg;
-pub use jpg::Jpg;
-#[allow(unused_imports)]
-pub use jpg::JpgOptions;
